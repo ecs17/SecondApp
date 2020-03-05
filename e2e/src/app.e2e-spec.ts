7226ee3c7e264e5ae1bfa98ccccb8d81335375d0
unknown <ecsrcm@gmail.com>
2020-03-05 14:26:24 -0600

    Inicial por formateo de pc

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('SecondApp app is running!');
+    expect(page.getTitleText()).toEqual('Welcome to casantstoreapp!');
   });
 
   afterEach(async () => {
